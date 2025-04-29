1792c4fab9e8a25ccc6415af699c6c444c8702f0
Savecoders <pablopincay2003@gmail.com>
2025-04-29 15:29:23 -0500

    feat: add scraper endpoint to fetch products

--- api/server.ts
+++ api/server.ts
@@ -1,4 +1,5 @@
 import express, { type Request, type Response, type NextFunction, type Express } from 'express';
+import getProductsScrape from './src/libs/products.scraping';
 const app: Express = express();
 
 app.use(express.json());
@@ -17,6 +18,22 @@ app.get('/', async (req: Request, res: Response, next: NextFunction): Promise<vo
     next(new Error((error as Error).message));
   }
 });
+
+// scraper fetch data
+app.get('/api/scrapper', async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const products = await getProductsScrape();
+    res.status(200).json({
+      message: 'Products fetched successfully',
+      success: true,
+      data: products,
+    });
+  } catch (error: unknown) {
+    next(new Error((error as Error).message));
+  }
+});
+
+// seed data
 
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
